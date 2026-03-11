c15efd014af3eac5241e96d623246905051c0d89
Aman Kumar <thisisaman408@gmail.com>
2026-03-11 08:30:42 +0530

    done

--- src/backend/outmate/__init__.py
+++ src/backend/outmate/__init__.py
@@ -1,3 +1,8 @@
-from .version.version import __version__
+from .version.version import get_version
+
+try:
+    __version__ = get_version()
+except ValueError:
+    __version__ = "unknown"
 
 __all__ = ["__version__"]
